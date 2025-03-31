d5735cacb8d4e310bfc858a0e76c5f1ad60c5c16
Seanthebeast300 <144385562+Seanthebeast300@users.noreply.github.com>
2025-03-31 19:11:35 -0400

    while loop to prevent user from inputting inproper values

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 //creates a random num 1-3 and chooses type based on that
 let rand = 0
 function getComputerChoice() {
@@ -21,7 +25,11 @@ function getComputerChoice() {
 let input = 0
 function getHumanChoice(){
     let output = ""
-    input = parseInt(prompt("Please enter either 1, 2, or 3. \n 1 = rock, 2 = paper, 3 = scissors"))
+    while(input <= 0 || input >= 4 || isNaN(input) == true)
+    {
+        input = parseInt(prompt("Please enter either 1, 2, or 3. \n 1 = rock, 2 = paper, 3 = scissors"))
+    }
+
 
     if (input === 1) {
         output = "rock"
@@ -32,11 +40,10 @@ function getHumanChoice(){
     if (input === 3) {
         output = "scissors"
     }
-    
+    console.log(input)
     return "Human chooses " + output
     
 }
 
-
-console.log(getComputerChoice())
 console.log(getHumanChoice())
+console.log(getComputerChoice())
