bb46cbb9659c1cd3bca08dc7178b2f49ac6f1417
Seanthebeast300 <144385562+Seanthebeast300@users.noreply.github.com>
2025-03-31 18:46:12 -0400

    computer choice method

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+function getComputerChoice() {
+    let output = "";
+    let rand = Math.round(((Math.random() * 2) + 1))
+    
+    if (rand === 1) {
+        output = "Rock"
+    }
+    if (rand === 2) {
+        output = "Paper"
+    }
+    if (rand === 3) {
+        output = "Scissors"
+    }
+}
