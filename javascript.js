4d615b737de177228169db726f7547915ef08fd9
Seanthebeast300 <144385562+Seanthebeast300@users.noreply.github.com>
2025-04-01 17:17:05 -0400

    changed the return values of choices to be numbers, will be calculated in playRound method

--- javascript.js
+++ javascript.js
@@ -5,45 +5,48 @@ let computerScore = 0;
 //creates a random num 1-3 and chooses type based on that
 let rand = 0
 function getComputerChoice() {
-    let output = "";
     rand = Math.round(((Math.random() * 2) + 1))
     
-    if (rand === 1) {
-        output = "rock"
-    }
-    if (rand === 2) {
-        output = "paper"
-    }
-    if (rand === 3) {
-        output = "scissors"
-    }
+    // if (rand === 1) {
+    //     output = "rock"
+    // }
+    // if (rand === 2) {
+    //     output = "paper"
+    // }
+    // if (rand === 3) {
+    //     output = "scissors"
+    // }
 
-    return "Computer chooses " + output
+    return rand
 }
 
 //prompts the user to enter a number 1-3, returns what was chosen based on number
 let input = 0
 function getHumanChoice(){
-    let output = ""
     while(input <= 0 || input >= 4 || isNaN(input) == true)
     {
         input = parseInt(prompt("Please enter either 1, 2, or 3. \n 1 = rock, 2 = paper, 3 = scissors"))
     }
 
 
-    if (input === 1) {
-        output = "rock"
-    }
-    if (input === 2) {
-        output = "paper"
-    }
-    if (input === 3) {
-        output = "scissors"
-    }
-    console.log(input)
-    return "Human chooses " + output
+    // if (input === 1) {
+    //     output = "rock"
+    // }
+    // if (input === 2) {
+    //     output = "paper"
+    // }
+    // if (input === 3) {
+    //     output = "scissors"
+    // }
+    
+    return input
     
 }
 
-console.log(getHumanChoice())
-console.log(getComputerChoice())
+function playRound(humanChoice, computerChoice) 
+{
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+
+    
+}
