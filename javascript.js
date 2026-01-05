ff48ed3ad13f67db329365aa3828a70f8d70ff93
Seanthebeast300 <144385562+Seanthebeast300@users.noreply.github.com>
2026-01-05 18:17:39 -0500

    removed old logic and implemented scoreboard that changed due to javascript code. Bugs with scoreboard

--- javascript.js
+++ javascript.js
@@ -3,6 +3,9 @@
 let humanScore = 0;
 let computerScore = 0;
 
+playerScore = document.getElementById("player-score")
+opponentScore = document.getElementById("computer-score")
+
 //creates a random num 1-3, returns number
 let rand = 0
 function getComputerChoice() {
@@ -11,67 +14,44 @@ function getComputerChoice() {
     return rand
 }
 
-//prompts the user to enter a number 1-3, returns number
-function getHumanChoice(){
-    let input = 0
-    while(input <= 0 || input >= 4 || isNaN(input) == true)
-    {
-        input = parseInt(prompt("Please enter either 1, 2, or 3. \n 1 = rock, 2 = paper, 3 = scissors \nHuman score: " + humanScore + "\nComputer score: " + computerScore))
-    }
-
-    return input 
-}
-
 function playRound(humanChoice, computerChoice) 
 {
-    if (humanChoice === computerChoice)
-    {
-        return "You picked the same item, it's a tie"
-    }
+    // if (humanChoice === computerChoice)
+    // {
+        
+    // }
     if (humanChoice === 1 && computerChoice === 2)
     {
         computerScore++;
-        return "Paper beats rock, computer wins"
     }
     if (humanChoice === 1 && computerChoice === 3)
     {
         humanScore++;
-        return "Rocks beats scissors, human wins"
     }
     if (humanChoice === 2 && computerChoice === 3)
     {
         computerScore++;
-        return "Scissors beats paper, computer wins"
     }
     if (humanChoice === 2 && computerChoice === 1)
     {
         humanScore++;
-        return "Paper beats rock, human wins"
     }
     if (humanChoice === 2 && computerChoice === 1)
     {
         humanScore++;
-        return "Paper beats rock, human wins"
     }
     if (humanChoice === 3 && computerChoice === 1)
     {
         computerScore++;
-        return "Rock beats scissors, computer wins"
     }
     if (humanChoice === 3 && computerChoice === 2)
     {
         humanScore++;
-        return "Scissors beats paper, human wins"
     }
+
+    playerScore.textContent = 'Player Score: ' + humanScore
+    opponentScore.textContent = 'Computer Score: ' + computerScore
 }
-
-// for (let i = 0; i < 5; i++)
-// {
-//     console.log(playRound(getHumanChoice(), getComputerChoice()));
-//     console.log("Human score: " + humanScore);
-//     console.log("Computer score: " + computerScore);
-// }
-
 
 // UI logic
 // element creation with selectors
@@ -79,12 +59,10 @@ rockImg = document.getElementById("rock")
 paperImg = document.getElementById("paper")
 scissorsImg = document.getElementById("scissors")
 
-
 //event listeners
 rockImg.addEventListener("click", chooseRock)
 paperImg.addEventListener("click", choosePaper)
 scissorsImg.addEventListener("click", chooseScissors)
-
 
 //functions triggered when image is clicked
 function chooseRock()
